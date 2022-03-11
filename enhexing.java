a81f1ad57c509d6a0eb00bf90fd7db8136a56309
Olegario, Ralph Huey Zapatero <ro68@students.waikato.ac.nz>
2022-03-11 16:34:57 +1300

    first commit

---
+++ enhexing.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.nio.file.Files;
+class enhexing
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+        File path = new File("Files/"+args[0]);
+        byte[] array = turnBytes(path);
+        File obj = new File("Copy of " + args[0]);
+        obj.createNewFile();
+        FileOutputStream fos = new FileOutputStream(obj);
+        fos.write(array);
+        fos.flush();
+        fos.close();
+        String fullBytes="";
+        for(byte b : array)
+        {
+            String st = String.format("%02X",b);
+            fullBytes+=st;
+        }
+        File byteFile = new File("byteOf"+args[0]);
+        byteFile.createNewFile();
+        FileWriter writer = new FileWriter(byteFile);
+        writer.write(fullBytes);
+        writer.flush();
+        writer.close();
+        System.out.println("\n"+fullBytes);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    static byte[] turnBytes(File file) throws IOException
+    {
+        FileInputStream fis = new FileInputStream(file);
+        byte[] arr = new byte[(int)file.length()];
+        fis.read(arr);
+        fis.close();
+        return arr;
+    }
+}
