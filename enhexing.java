f7c5f1560709264ddff6b132a4277d0e77601a27
TikiTikiPlus <nootherthanme65@gmail.com>
2022-03-14 15:03:10 +1300

    changed algorithm

--- enhexing.java
+++ enhexing.java
@@ -15,16 +15,35 @@ class enhexing
         String fullBytes="";
         for(byte b : array)
         {
-            String st = String.format("%02X",b);
-            fullBytes+=st;
+            String st = String.format("%8s",Integer.toBinaryString(b&0XFF)).replace(' ', '0');
+            System.out.println(st);
+            if(fullBytes!="")
+            {
+                fullBytes+=","+st;
+            }
+            else
+            {
+                fullBytes += st;
+            }
+        }
+        String[] arrayFullBytes = fullBytes.split(",",-1);
+        String fullWord="";
+        for(String s: arrayFullBytes)
+        {
+            long binaryBytes = Long.parseLong(s);
+            int decimalBytes = binaryToDecimal(binaryBytes);
+            String fullHex = decimalToHex(decimalBytes);
+            fullWord+=fullHex;
+            System.out.println(fullWord);
+            System.out.println(s);
         }
         File byteFile = new File("byteOf"+args[0]);
         byteFile.createNewFile();
         FileWriter writer = new FileWriter(byteFile);
-        writer.write(fullBytes);
+        writer.write(fullWord);
         writer.flush();
         writer.close();
-        System.out.println("\n"+fullBytes);
+        System.out.println("\n"+fullWord);
         }
         catch(Exception e)
         {
@@ -38,5 +57,21 @@ class enhexing
         fis.read(arr);
         fis.close();
         return arr;
+    }
+    static int binaryToDecimal(long binary)
+    {
+        int decimalNumber = 0, i=0;
+        while(binary>0)
+        {
+            decimalNumber += Math.pow(2,i++)*(binary%10);
+            binary /= 10;
+        }
+        return decimalNumber;
+    }
+    static String decimalToHex(int binaryText)
+    {
+        String hexa = Integer.toHexString(binaryText);
+        hexa = hexa.toUpperCase();
+        return hexa;
     }
 }
